8877ce774a4efc7b4889773ab9fbaae07d5cb173
luochun <luochun@idcos.com>
2022-06-21 10:55:15 +0800

    Updates

---
+++ component---src-pages-privacy-js-4dab15658d31812c89c0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[844],{8897:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(7294),a=n(8457);function l(){return r.createElement(r.Fragment,null,r.createElement(a.pQ,{title:"Privacy Policy",useTitleTemplate:!0,noIndex:!0}),r.createElement(a.T3,null,r.createElement(a.Ek,{sectionId:"privacy",heading:"Privacy Policy"})))}}}]);
+//# sourceMappingURL=component---src-pages-privacy-js-4dab15658d31812c89c0.js.map
