e04cd12b2c8667833c61b009cc053b8c76ad65d0
luochun <luochun@idcos.com>
2022-06-21 11:29:53 +0800

    update: 首屏文案

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="luo's blog" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
